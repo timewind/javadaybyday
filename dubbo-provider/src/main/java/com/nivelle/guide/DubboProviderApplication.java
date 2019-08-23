ce93be3eda0401e9e4e152a0e64001bbd8d65030
nivellefu <fuxinzhong@zhangyue.com>
2019-08-23 15:39:26 +0800

    【readme】

--- dubbo-provider/src/main/java/com/nivelle/guide/DubboProviderApplication.java
+++ dubbo-provider/src/main/java/com/nivelle/guide/DubboProviderApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableDubbo
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@EnableAsync
 public class DubboProviderApplication {
 
     public static void main(String[] args) {
