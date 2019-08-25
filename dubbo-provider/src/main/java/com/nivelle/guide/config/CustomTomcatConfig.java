c0f66029e86485a4cf0b834cdca5c84879761f53
nivellefu <fuxinzhong@zhangyue.com>
2019-08-25 14:52:51 +0800

    【自定义启动Tomcat】

--- dubbo-provider/src/main/java/com/nivelle/guide/config/CustomTomcatConfig.java
+++ dubbo-provider/src/main/java/com/nivelle/guide/config/CustomTomcatConfig.java
@@ -1,10 +1,13 @@
 package com.nivelle.guide.config;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
 
 /**
  * 自定义项目启动Servlet
@@ -13,10 +16,17 @@ import org.springframework.stereotype.Component;
  * @date 2019/08/25
  */
 @Component
-public class CustomServletConfig implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+public class CustomTomcatConfig implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
     @Override
     public void customize(ConfigurableServletWebServerFactory server) {
-        server.setDisplayName("nivelle dubbo provider test");
+        ((TomcatServletWebServerFactory) server).addConnectorCustomizers((Connector connector) -> {
+            Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+            protocol.setMaxConnections(200);
+            protocol.setMaxThreads(200);
+            protocol.setSelectorTimeout(3000);
+            protocol.setSessionTimeout(3000);
+            protocol.setConnectionTimeout(3000);
+        });
     }
 
     @Bean
