8fa9aac20f46b289e2fce4a5466eda23011ed9d7
nivellefu <fuxinzhong@zhangyue.com>
2019-10-15 19:49:37 +0800

    【数据结构】

--- src/main/java/com/nivelle/guide/datastructures/HashSetDemo.java
+++ src/main/java/com/nivelle/guide/datastructures/HashSetDemo.java
@@ -12,5 +12,21 @@ public class HashSetDemo {
 
     public static void main(String[] args) {
         HashSet hashSet = new HashSet();
+
+        boolean aResult = hashSet.add("a");
+        System.out.println("aResult is: " + aResult);
+
+        boolean bResult = hashSet.add("b");
+        System.out.println("bResult is: " + bResult);
+
+        boolean cResult = hashSet.add("a");
+        System.out.println("cResult is: " + cResult);
+
+        java.util.Iterator iterator = hashSet.iterator();
+        while (iterator.hasNext()) {
+            System.err.print(iterator.next());
+        }
+
+
     }
 }
---
+++ src/main/java/com/nivelle/guide/datastructures/LinkedTreeMapDemo.java
@@ -0,0 +1,14 @@
+package com.nivelle.guide.datastructures;
+
+import com.google.gson.internal.LinkedTreeMap;
+
+/**
+ * linkedTreeMap
+ *
+ * @author fuxinzhong
+ * @date 2019/10/15
+ */
+public class LinkedTreeMapDemo {
+
+    LinkedTreeMap linkedTreeMap = new LinkedTreeMap();
+}
