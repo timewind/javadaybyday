ab91b5f79b2067afb48cd8a43039db1b96fecd04
nivellefu <fuxinzhong@zhangyue.com>
2019-10-15 20:04:40 +0800

    【数据结构】

--- src/main/java/com/nivelle/guide/datastructures/CopyOnWriterArrayListDemo.java
+++ src/main/java/com/nivelle/guide/datastructures/CopyOnWriterArrayListDemo.java
@@ -15,10 +15,25 @@ public class CopyOnWriterArrayListDemo {
 
 
     public static void main(String[] args) {
+
+        /**
+         *  1. 内部持有一个ReentrantLock lock = new ReentrantLock();
+         *
+         *  2. 底层是用volatile transient声明的数组 array
+         *
+         *  3. 读写分离，写时复制出一个新的数组，完成插入、修改或者移除操作后将新数组赋值给array
+         */
         CopyOnWriteArrayList copyOnWriteArrayList = new CopyOnWriteArrayList();
 
         CopyOnWriteArraySet copyOnWriteArraySet = new CopyOnWriteArraySet();
 
         CopyOnWriteMap copyOnWriteMap = new CopyOnWriteMap();
+
+        copyOnWriteArrayList.add(1);
+        copyOnWriteArrayList.add(2);
+        System.out.println(copyOnWriteArrayList);
+
+        Object element = copyOnWriteArrayList.get(1);
+        System.out.println(element);
     }
 }
--- src/main/java/com/nivelle/guide/datastructures/StackDemo.java
+++ src/main/java/com/nivelle/guide/datastructures/StackDemo.java
@@ -12,5 +12,24 @@ public class StackDemo {
 
     public static void main(String[] args) {
         Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        System.out.println("stack1 is: " + stack);
+
+        Object popResult = stack.pop();
+        System.out.println("pop is:" + popResult);
+        System.out.println("after pop stack2 is:" + stack);
+
+        stack.push(2);
+        Object peekResult1 = stack.peek();
+        System.out.println("peek result1 is:" + peekResult1);
+
+        Object peekResult2 = stack.peek();
+        System.out.println("peek result2 is:" + peekResult2);
+
+        System.out.println("stack3 is: " + stack);
+
+        Object firstElement = stack.firstElement();
+        System.out.println("firstElement is:" + firstElement);
     }
 }
