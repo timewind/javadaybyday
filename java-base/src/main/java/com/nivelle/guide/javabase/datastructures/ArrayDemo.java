55b71183158153ef8b61b94f1f393ce51e13514d
nivellefu <fuxinzhong@zhangyue.com>
2019-10-30 00:15:32 +0800

    [Arrays.asList()方法的注意点]

--- java-base/src/main/java/com/nivelle/guide/javabase/datastructures/ArrayDemo.java
+++ java-base/src/main/java/com/nivelle/guide/javabase/datastructures/ArrayDemo.java
@@ -11,6 +11,17 @@ import java.util.Arrays;
 public class ArrayDemo {
 
     public static void main(String[] args) {
+
+
+        /**
+         *     严格意义上说返回的是数组的一个视图
+         *
+         *    （1）该方法不适用于基本数据类型（byte,short,int,long,float,double,boolean）
+         *
+         * 　　（2）该方法将数组与列表链接起来，当更新其中之一时，另一个自动更新
+         *
+         * 　　（3）不支持add和remove方法
+         */
         Arrays.asList();
     }
 
