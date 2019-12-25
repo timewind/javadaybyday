239a22734840b9bed0aba191306c6b69ec863656
nivellefu <fuxinzhong@zhangyue.com>
2019-12-25 17:04:13 +0800

    【整理】

---
+++ springboot-all/src/main/java/com/nivelle/springbootall/SpringbootAllApplication.java
@@ -0,0 +1,13 @@
+package com.nivelle.springbootall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAllApplication.class, args);
+    }
+
+}
---
+++ springboot-all/src/test/java/com/nivelle/springbootall/SpringbootAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nivelle.springbootall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
